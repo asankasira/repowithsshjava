a89036ffe26b547bf099e99507ba6257dbeea7d4
Asanka Siriwardena <asankabg@gmail.com>
2014-11-26 22:52:08 +0530

    new package

---
+++ src/com/asanka/LoopTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.asanka;
+
+/**
+ * @author Asanka Siriwardena
+ *
+ */
+public class LoopTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		int[] arr = {4, 6, 7, 2, 13, 9, 5};
+		
+		for (int i : arr) {
+			System.out.println(i);
+		}
+	}
+
+}
