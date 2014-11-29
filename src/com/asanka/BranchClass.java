7cde3102d8fad25bf09f7b7081849ed1538fc2fb
Asanka Siriwardena <asankabg@gmail.com>
2014-11-29 19:58:44 +0530

    new class added for branch

---
+++ src/com/asanka/BranchClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.asanka;
+
+/**
+ * @author Asanka Siriwardena
+ *
+ */
+public class BranchClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Only Avaliable in Branch");
+	}
+
+}
