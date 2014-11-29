9af66428d2dcc4edc4204302de9ae165410aff44
Asanka Siriwardena <asankabg@gmail.com>
2014-11-29 19:41:09 +0530

    suck it

--- src/com/asanka/LoopTest.java
+++ src/com/asanka/LoopTest.java
@@ -3,6 +3,12 @@
  */
 package com.asanka;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 /**
  * @author Asanka Siriwardena
  *
@@ -14,13 +20,48 @@ public class LoopTest {
 	 */
 	public static void main(String[] args) {
 		
-		int[] arr = {4, 6, 7, 2, 13, 9, 5};
-		
-		for (int i : arr) {
-			System.out.println(i);
+//		int[] arr = {4, 6, 7, 2, 13, 9, 5};
+//		
+//		for (int i : arr) {
+//			System.out.println(i);
+//		}
+//		
+//	    System.out.println("done here!");
+
+	    List<String> list = new ArrayList();
+	    list.add("Mon");
+	    list.add("Tue");
+	    list.add("Wed");
+	    list.add("Thu");
+	    list.add("Fri");
+	    list.add("Sat");
+	    list.add("Sun");
+	    
+	    
+	    System.out.println(list);
+	    
+	    int i = 0;
+	    for (ListIterator<String> iterator = list.listIterator(); iterator.hasNext();) {
+			
+	    	   i++;
+	    	   String entry = iterator.next();
+	    	   if("Wed".equals(entry)){
+	    		   iterator.remove();
+	    		   
+	    		   iterator.add("Nwed");
+	    		   iterator.add("Poya");
+	    		   iterator.add("Wed");
+	    	   }
+			
 		}
-		
-	    System.out.println("done here!");
+	    
+	    for (String string : list) {
+			
+	    	if("Wed".equals(string))
+	    		list.remove(string);
+		}
+	    
+	    System.out.println(list + " Executions: " + i);
 	}
 
 }
